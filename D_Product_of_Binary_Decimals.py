fcc2b3e630f6ece0ea5769cd91b36590e73b02e4
Fawcet Jenusdy Makay <jenusdy@gmail.com>
2024-04-01 23:54:27 +0700

    Basics CPP

--- D_Product_of_Binary_Decimals.py
+++ D_Product_of_Binary_Decimals.py
@@ -1,39 +1,21 @@
-test = int(input())
+MAX = 100_007
+MOD = 1_000_000_007
 
-def is_binary_string(s):
-    return all(char in {'0', '1'} for char in s)
+def is_good(n):
+  if n == 1:
+    return True
 
-for t in range(0,test):
-    n = int(input())
+  binary_decimals = [i for i in range(2, MAX) if all(digit <= str(1) for digit in str(i))]  # Efficiently generate binary decimals
+  for divisor in binary_decimals:
+    if n % divisor == 0 and is_good(n // divisor):
+      return True
+  return False
 
-    check = True
-    if is_binary_string(str(n)):
-        print("YES")
-    else:
-        hasil = ""
-        s = 0
-        while(n > 11):
-            h = n // 11
-            s = n % 11
+def solve():
+  n = int(input())
+  print("YES" if is_good(n) else "NO")
 
-            temp = str(h) + str(s)
-            hasil += str(h)
-
-            if h > s :
-                n = h
-            else :
-                n = s
-
-            if is_binary_string(temp):
-                check = False
-                print("YES")
-                break
-        
-        
-        if check : 
-            temp_ = hasil + str(s)
-            if is_binary_string(temp_):
-                print("YES")
-            else :
-                print("NO")
-    
+if __name__ == "__main__":
+  t = int(input())
+  for _ in range(t):
+    solve()
---
+++ basic-cpp/HelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    // This is a commment
+    std::cout << "Hello World" << std::endl;
+    std::cout << "Its's really Good" << "\n";
+
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+
+    std::cout << "Nilai X : " << x << "\n";
+    std::cout << "Nilai Y : " << y << "\n";
+    std::cout << "Hasil Jumlah : " << sum;
+    return 0;
+}
+
+// https://www.youtube.com/watch?v=-TkoO8Z07hI
