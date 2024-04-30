9e8f208ac9a29398c875dc73f034a9668e559750
Fawcet Jenusdy Makay <jenusdy@gmail.com>
2024-04-30 12:22:31 +0700

    B_Queue_at_the_School

--- B_Queue_at_the_School.py
+++ B_Queue_at_the_School.py
@@ -1,7 +1,16 @@
 n, t = input().split(' ')
-n, t = list(map(int, [n,t ]))
+n, t = list(map(int, [n, t]))
 
-q = input()
+q = list(input())
 
-for i in q:
-    print(i)
+
+for i in range(t):
+    flag = False
+    for index in range(len(q) - 1):
+        if q[index] == 'B' and q[index + 1] == 'G' and not flag:
+            q[index], q[index + 1] = q[index + 1], q[index]
+            flag = True
+        else: 
+            flag = False
+
+print(''.join(q))
